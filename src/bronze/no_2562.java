a4d4f2352867ef77a21c27869f13b16519a42494
LAPTOP-V2I2K4EI\dynii <dyniiyeyo@naver.com>
2025-07-30 21:40:15 +0900

    250730 renewal

--- src/bronze/no_2562.java
+++ src/bronze/no_2562.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class no_2562 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int x = 0;
+        int temp = 0;
 
+        for (int i = 1; i < 10; i++) {
+            int a = sc.nextInt();
+            if (a > temp) {
+                temp = a;
+                x = i;
+            }
+        }
+        System.out.println(temp);
+        System.out.println(x);
     }
 }
